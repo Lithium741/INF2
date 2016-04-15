af4d096e04f06855662a5399d7e544ad1b59914f
Manuel Schweizer <manuel.schweizer1@yahoo.de>
2016-04-15 19:53:00 +0200

    Added GUI and Dateiverwaltung classes

---
+++ src/Vokabelkram/Dateiverwaltung.java
@@ -0,0 +1,31 @@
+package Vokabelkram;
+
+import java.util.ArrayList;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ConcurrentModificationException;
+import javax.swing.*;
+
+public class Dateiverwaltung extends JFrame {
+
+	public static ArrayList<Speicher> speicher = new ArrayList<Speicher>();
+
+	public static String loeschen(JTextField wort, int b) {
+			for (Speicher temp : StartGui.speicher) {
+				if (temp.deutsch.equals(wort.getText()) || temp.english.equals(wort.getText())) {
+					if (b == 0) {
+						speicher.remove(temp);
+						return temp.deutsch + " = " + temp.english + " gelöscht!";
+					} else {
+
+					}
+				} else {
+					return "Wort nicht gefunden!";
+				}
+			}
+		return "test";
+	}
+}
