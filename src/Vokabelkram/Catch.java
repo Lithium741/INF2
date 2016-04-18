903701075856aa69b76acb2b4d7b49738ec11058
Manuel Schweizer_Tablet <manuel.schweizer1@yahoo.de>
2016-04-18 12:33:11 +0200

    Started developement of catch

---
+++ src/Vokabelkram/Catch.java
@@ -0,0 +1,10 @@
+package Vokabelkram;
+
+import javax.swing.JPanel;
+
+public class Catch extends JPanel{
+	
+	private int x = 0;
+	private int y = 0;
+
+}
---
+++ src/Vokabelkram/FallingWord.java
@@ -0,0 +1,11 @@
+package Vokabelkram;
+
+public class FallingWord {
+	private String deutsch;
+	private String englisch;
+
+	public FallingWord(String deutsch, String englisch) {
+		this.deutsch = deutsch;
+		this.englisch = englisch;
+	}
+}
